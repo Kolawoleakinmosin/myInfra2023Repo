00caa5da850c775e862ac863621acd0355525496
Akinmosin Kolawole Samuel <118081096+Kolawoleakinmosin@users.noreply.github.com>
2023-01-13 14:43:04 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-newclasst"
+    bucket = "my-s3-bucket-newclass"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-table-new"
